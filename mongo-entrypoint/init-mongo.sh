7c562732eb78ba9d12e8407462b4e37f168cc9a1
banbanpeppa <chenzhiling01@corp.netease.com>
2022-09-14 21:14:34 +0800

    :sparkles: change the init-mongo.sh

--- mongo-entrypoint/init-mongo.sh
+++ mongo-entrypoint/init-mongo.sh
@@ -1,49 +1,19 @@
 set -e
 
 mongo <<EOF
-use admin
 
-db.createUser({
-    user: '${MONGO_SUPER_ROOT_USERNAME}',
-    pwd: '${MONGO_SUPER_ROOT_PASSWORD}',
-    roles: [{ 
-        role: "readWrite", 
-        db: "admin" 
-    }, 
-    "readWriteAnyDatabase",
-    "userAdminAnyDatabase", 
-    "dbAdminAnyDatabase"
-    ]
-  }
-);
-
-use loghub
+use ${MONGO_TESTDB_DATABASE}
 db.createUser(
     {
-        user: '${MONGO_LOGHUB_USERNAME}',
-        pwd: '${MONGO_LOGHUB_PASSWORD}',
+        user: '${MONGO_TESTDB_USERNAME}',
+        pwd: '${MONGO_TESTDB_PASSWORD}',
         roles: [
             { 
                 role: "readWrite", 
-                db: '${MONGO_LOGHUB_DATABASE}'
+                db: '${MONGO_TESTDB_DATABASE}'
             }
         ],
         passwordDigestor: "server",
-    }
-);
-
-use logstream
-db.createUser(
-    {
-        user: '${MONGO_LOGSTREAM_USERNAME}',
-        pwd: '${MONGO_LOGSTREAM_PASSWORD}',
-        roles: [
-            {
-                role: "readWrite", 
-                db: '${MONGO_LOGSTREAM_DATABASE}',
-            }
-        ],
-        passwordDigestor: "server"
     }
 );
 EOF
